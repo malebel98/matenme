5e8e59b3943e2c1a716daefd933a21ac1d9f2a07
malebel98 <malebel98@gmail.com>
2017-11-09 22:19:49 -0500

    1
    Guardar

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+   public void NewGamebtn (string newgamegetaway)
+    {
+        SceneManager.LoadScene(newgamegetaway);
+    }
+
+    public void ExitGamebtn()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseBtn, pauseMenu, slider;
+
+    public void OnPause()
+    {
+        pauseMenu.SetActive(true);
+        slider.SetActive(false);
+        pauseBtn.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void OffPause()
+    {
+        pauseMenu.SetActive(false);
+        slider.SetActive(true);
+        pauseBtn.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu(string backtomenu)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(backtomenu);
+    }
+
+    public void ExitGamebtn()
+    {
+        Application.Quit();
+    }
+}
